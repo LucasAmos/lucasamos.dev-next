b729dcf270dca550392eff299b18628cc0adab2c
Lucas Amos <ldjamos@icloud.com>
2024-06-26 22:32:09 +0100

    remove prism cdn

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/prism-themes/1.9.0/prism-vsc-dark-plus.min.css"
-        />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
