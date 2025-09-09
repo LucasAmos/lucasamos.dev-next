aac933d5e1e5cfbc508225dc50ab0f688e5559d1
Lucas Amos <9055266+LucasAmos@users.noreply.github.com>
2025-09-09 11:09:29 +0100

    Update page.tsx

--- src/app/reading/page.tsx
+++ src/app/reading/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Metadata } from "next";
 import { BOOKS_QUERY } from "../../sanity/queries/books";
 import { client } from "../../sanity/lib/client";
 
@@ -7,12 +8,23 @@ import { BOOKS_QUERYResult } from "../../../sanity.types";
 
 export const revalidate = 0;
 
+export const metadata: Metadata = {
+  title: "Lucas Amos - What I'm reading",
+  description: "What I'm reading",
+  openGraph: {
+    title: "Lucas Amos - What I'm reading",
+    description: "What I'm reading",
+    authors: ["Lucas Amos"],
+    images: ["https://www.lucasamos.dev/images/lucas.JPG"],
+  },
+};
+
 const Books: React.FC = async () => {
   const books: BOOKS_QUERYResult = await client.fetch(BOOKS_QUERY);
 
   return (
     <>
-      <h1 className="mb-5 font-Inter text-2xl">My reading list</h1>
+      <h1 className="mb-5 font-Inter text-2xl">What I'm reading</h1>
 
       <div className="grid grid-flow-row xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2">
         {books.map((book) => {
