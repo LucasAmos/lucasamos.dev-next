49461b179e5047b1e3ba8e4de8f800a7481dbfd2
Lucas Amos <9055266+LucasAmos@users.noreply.github.com>
2025-09-13 21:06:46 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,22 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 import "../styles/global.css";
-// The following import prevents a Font Awesome icon server-side rendering bug,
-// where the icons flash from a very large icon down to a properly sized one:
-import "@fortawesome/fontawesome-svg-core/styles.css";
-// Prevent fontawesome from adding its CSS since we did it manually above:
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
+
+const links = [
+  ["Posts", "/"],
+  ["About me", "/about"],
+  ["Contact", "/contact"],
+];
 
 export default function Navbar(): JSX.Element {
+  const [open, setOpen] = useState(false);
   return (
     <>
-      <nav className="float-right m-4 flex space-x-4 sm:m-4 sm:justify-center">
-        {[
-          ["Posts", "/"],
-          ["About me", "/about"],
-          ["Contact", "/contact"],
-        ].map(([title, url]) => (
+      <nav className="hidden  float-right m-4 sm:flex space-x-4 sm:m-4 sm:justify-center">
+        {links.map(([title, url]) => (
           <Link
             key={title}
             href={url}
@@ -26,7 +26,33 @@ export default function Navbar(): JSX.Element {
             <span className="block h-1 max-w-0  bg-purple-700 transition-all duration-500 md:group-hover:max-w-full"></span>
           </Link>
         ))}
-      </nav>{" "}
+      </nav>
+      <nav className="float-right space-x-4 m-4 sm:justify-center">
+        <div className="float-right">
+          <FontAwesomeIcon
+            icon={open ? faClose : faBars}
+            className="sm:hidden"
+            onClick={() => {
+              console.log("clicked");
+              setOpen(!open);
+            }}
+          />
+        </div>
+        {open && (
+          <div className="mt-6 ">
+            {[...links, ["What I'm reading", "/reading"]].map(([title, url]) => (
+              <Link
+                key={title}
+                href={url}
+                className="group rounded-lg text-xl font-medium text-slate-700 transition duration-300 md:!mr-[50px]"
+              >
+                {title}
+                <span className="block h-1 max-w-0  bg-purple-700 transition-all duration-500 md:group-hover:max-w-full"></span>
+              </Link>
+            ))}
+          </div>
+        )}
+      </nav>
       <div className="clear-both">
         <hr />
       </div>
