d3b455d4761bd72a1b0446fb33ef28603372a4a1
Lucas Amos <9055266+LucasAmos@users.noreply.github.com>
2026-04-06 19:19:25 +0100

    skills icon

--- src/components/techStack/index.tsx
+++ src/components/techStack/index.tsx
@@ -1,9 +1,10 @@
 import type { ReactNode } from "react";
-import { TechStackSection } from "../techStackSection";
+import { Skills } from "../skills";
 
 type ITechStack = {
+  icon: string | null;
+  skills: Array<string>;
   title: string;
-  skills: string[];
 };
 
 export function TechStack({ stack }: { stack: ITechStack[] }): ReactNode {
@@ -11,8 +12,8 @@ export function TechStack({ stack }: { stack: ITechStack[] }): ReactNode {
     <>
       <h2 className="text-2xl font-bold mt-10">Tech Stack</h2>
       <div className="mt-5 mb-10 gap-2 grid grid-flow-row  grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-        {stack.map(({ title, skills }) => (
-          <TechStackSection key={title} title={title} skills={skills} />
+        {stack.map(({ title, skills, icon }) => (
+          <Skills icon={icon} key={title} title={title} skills={skills} />
         ))}
       </div>
     </>
