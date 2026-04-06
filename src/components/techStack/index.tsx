520d3d03e2e8e1e2dc6d12f02af85b06a0b3dfe9
Lucas Amos <9055266+LucasAmos@users.noreply.github.com>
2026-04-06 20:21:20 +0100

    update skills layout

--- src/components/techStack/index.tsx
+++ src/components/techStack/index.tsx
@@ -11,7 +11,7 @@ export function TechStack({ stack }: { stack: ITechStack[] }): ReactNode {
   return (
     <>
       <h2 className="text-2xl font-bold mt-10">Tech Stack</h2>
-      <div className="mt-5 mb-10 gap-2 grid grid-flow-row  grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+      <div className="mt-5 mb-10 gap-2 grid grid-flow-row grid-cols-1 sm:grid-cols-2  md:grid-cols-3">
         {stack.map(({ title, skills, icon }) => (
           <Skills icon={icon} key={title} title={title} skills={skills} />
         ))}
